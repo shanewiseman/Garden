dba85fcbef07e957114a2e800377a20090aefbd6
Shane Wiseman <shane.db.wiseman@gmail.com>
2015-12-10 23:54:19 -0500

    Database Integration
    	modified:   Garden/settings.py
    	modified:   GardenServer/models.py
    	GardenServer/migrations/0001_initial.py
    	GardenServer/migrations/0002_auto_20151211_0436.py
    	GardenServer/migrations/0003_auto_20151211_0446.py

--- GardenServer/models.py
+++ GardenServer/models.py
@@ -2,4 +2,25 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Device ( models.Model ):
+    name        = models.CharField( max_length = 255 )
+    password    = models.CharField( max_length = 32 )
+    description = models.CharField( blank = True , max_length = 255 )
+    created     = models.DateTimeField(auto_now=True)
+
+class DataType ( models.Model ):
+    name        = models.CharField( max_length = 255 )
+    description = models.CharField( blank = True , max_length = 255 )
+
+class Data ( models.Model ):
+    created     = models.DateField()
+    value       = models.TextField()
+    device      = models.ForeignKey( Device , on_delete=models.CASCADE )
+    datatype    = models.ForeignKey( DataType , on_delete=models.CASCADE )
+
+class Response ( models.Model ):
+    created     = models.DateTimeField(auto_now=True)
+    value       = models.TextField()
+    device      = models.ForeignKey( Device , on_delete=models.CASCADE )
+
+
